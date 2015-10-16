3bf5042c1b55bc9ac7bed2813566b194cbccd4cd
Wojtek Nowak <vojtek.nowak@gmail.com>
2015-10-17 00:09:55 +0200

    fixed 3 step

--- 3/solver.py
+++ 3/solver.py
@@ -1,14 +1,23 @@
 import re
 import heapq
+import sys
+import md5
 
 
-re_rect = re.compile('\<rect\s+fill="(?P<type>\w+)"\s+height="(?P<height>\d+)"\s+width="(?P<width>\d+)"\s+x="(?P<x>\d+)"\s+y="(?P<y>\d+)"><\/rect>')
+re_rect = re.compile('\<rect\s+fill="(?P<type>\w+)"\s+height="(?P<height>\d+)"\s+width="(?P<width>\d+)"\s+x="(?P<x>\d+)"\s+y="(?P<y>\d+)"')
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+else:
+    file_name = 'maze.html'
 
-maze_html = open('maze.html', 'r')
+maze_html = open(file_name, 'r')
 
 walls = []
 red = None
 green = None
+
+maze_width = 0
+maze_height = 0
 for type, height, width, x, y in re_rect.findall(maze_html.read()):
     x = int(x)
     y = int(y)
@@ -19,10 +28,8 @@ for type, height, width, x, y in re_rect.findall(maze_html.read()):
         green = (x/height, y/height)
     elif type == 'red':
         red = (x/height, y/height)
-
-
-print walls
-
+    maze_width = max(maze_width, x/height)
+    maze_height = max(maze_height, y/height)
 
 
 
@@ -41,32 +48,40 @@ class Cell(object):
         self.g = 0
         self.h = 0
         self.f = 0
+
     def __repr__(self):
         return '%s,%s - %s' %(self.x, self.y, self.reachable)
 
 class AStar(object):
-    def __init__(self):
+    def __init__(self, walls, start, end, grid_height, grid_width):
         self.opened = []
         heapq.heapify(self.opened)
         self.closed = set()
         self.cells = []
-        self.grid_height = 6
-        self.grid_width = 6
+        self.walls = walls
+        self.start = start
+        self.end = end
 
-    def init_grid(self, walls, start, end):
+        self.grid_height = grid_height
+        self.grid_width = grid_width
+
+        print self.grid_height
+        print self.grid_width
+
+    def init_grid(self):
         # walls = ((0, 5), (1, 0), (1, 1), (1, 5), (2, 3),
         #      (3, 1), (3, 2), (3, 5), (4, 1), (4, 4), (5, 1))
         for x in range(self.grid_width):
             for y in range(self.grid_height):
-                if (x, y) in walls:
+                if (x, y) in self.walls:
                     reachable = False
                 else:
                     reachable = True
                 self.cells.append(Cell(x, y, reachable))
-        self.start = self.get_cell(*start)
-        self.end = self.get_cell(*end)
-        # self.start = self.get_cell(0, 0)
-        # self.end = self.get_cell(5, 5)
+        print self.start
+        print self.end
+        self.start = self.get_cell(*self.start)
+        self.end = self.get_cell(*self.end)
 
     def get_heuristic(self, cell):
         """
@@ -84,7 +99,6 @@ class AStar(object):
         @param y cell y coordinate
         @returns cell
         """
-        print self.cells
         return self.cells[x * self.grid_height + y]
 
     def get_adjacent_cells(self, cell):
@@ -105,11 +119,32 @@ class AStar(object):
             cells.append(self.get_cell(cell.x, cell.y + 1))
         return cells
 
+    def what_move(self, old, new):
+
+        if old.x == new.x:
+            if old.y > new.y:
+                return 'B'
+            else:
+                return 'T'
+        else:
+            if old.x > new.x:
+                return 'R'
+            else:
+                return 'L'
+
+
     def display_path(self):
+        steps = []
         cell = self.end
         while cell.parent is not self.start:
+            old, new = cell.parent, cell
+            steps.append(self.what_move(old, new))
             cell = cell.parent
-            print 'path: cell: %d,%d' % (cell.x, cell.y)
+            # print 'path: cell: %d,%d' % (cell.x, cell.y)
+
+        steps.append(self.what_move(self.start, cell))
+
+        return steps
 
     def compare(self, cell1, cell2):
         """
@@ -145,8 +180,8 @@ class AStar(object):
             self.closed.add(cell)
             # if ending cell, display found path
             if cell is self.end:
-                self.display_path()
-                break
+                return self.display_path()
+                # break
             # get adjacent cells for cell
             adj_cells = self.get_adjacent_cells(cell)
             for adj_cell in adj_cells:
@@ -163,8 +198,14 @@ class AStar(object):
                         heapq.heappush(self.opened, (adj_cell.f, adj_cell))
 
 
-a = AStar()
-a.init_grid(set(walls), green, red)
-a.process()
+a = AStar(walls, red, green, maze_height, maze_width)
+a.init_grid()
+steps = a.process()
+
+steps = "".join(steps)
+
+print steps
+
+print md5.md5(steps).hexdigest()
 
 
